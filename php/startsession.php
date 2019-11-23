a0345f92f51935e6f160566be7e5d1eb79481546
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2019-11-23 14:20:26 +0000

    Update startsession.php

--- php/startsession.php
+++ php/startsession.php
@@ -1,17 +1,1 @@
 <?php
-$str = "dbname=postgres user=postgres password=postgres host=localhost port=5432";
-$connection = pg_connect($str);
-
-if (!$connection) {
-    die("Erro na ligacao");
-}
-echo "Ligacao estabelecida!";
-
-$username = $_POST['username'];
-$password = $_POST['confirmpassword'];
-$email = $_POST['email'];
-
-pg_close($connection);
-
-header('Location: ../homepage.php');
-?>
