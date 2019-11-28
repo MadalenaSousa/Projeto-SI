ed52d116aa0adc09cc4112dce1ce0ae5977bd0e7
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2019-11-28 16:31:27 +0000

    ...

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>LDMEats | Sign Up</title>
+    <title>LDMEats | Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="images/icon-logo.png">
--- php/sendconfirmationmail.php
+++ php/sendconfirmationmail.php
@@ -9,6 +9,7 @@ Este mail serve para confirmar o seu email. Por favor, nao responda a este mail.
 BEM VINDO AO LDMEats!
 Faça Login na plataforma com a sua nova conta através do seguinte link:
 https://student.dei.uc.pt/~msousa/Projeto-SI/login.php
+https://student.dei.uc.pt/~avicente/Projeto-SI/login.php
  
 Cumprimentos,
 LDMEats
