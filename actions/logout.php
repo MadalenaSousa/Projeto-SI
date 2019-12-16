f92a258d8b4b13560b6bb1fa60faceba60af7818
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2019-12-16 04:27:56 +0000

    cenas

--- actions/logout.php
+++ actions/logout.php
@@ -1,6 +1,9 @@
 <?php
 
 if(isset($_SESSION['username'])) {
+
+    include dirname(__FILE__) . '/../database-data-functions/close-connection.php';
+
     unset($_SESSION);
     session_destroy();
 }
---
+++ database-data-functions/close-connection.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once(dirname(__FILE__) . '/connection.php');
+
+pg_close(getDBConnection());
